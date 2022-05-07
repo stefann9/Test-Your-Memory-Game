67009ed6a15b4b255c6c8fa4ce5525c9bc96bd4e
stef <stefan.manea198@gmail.com>
2022-05-07 17:59:25 +0300

    fix overlap

--- app.js
+++ app.js
@@ -1,82 +1,73 @@
 const container = document.querySelector('.container');
 
-function makeRand(min,max){
+function makeRand(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-// const div1 = document.createElement('div')
+let tSize = 38;
+let tCount = 20;
+// impartim containerul in spati disponibile pe rand si coloane pentru 
+// grosimea si inaltimea numerelor
+let numRow = Math.floor(container.offsetWidth/tSize);
+let numCol = Math.floor(container.offsetHeight/tSize);
+let spacesOnRow = [];
+let spacesOnCol = [];
 
-// randLeft = makeRand(0,container.offsetWidth) + 'px'
-// randTop = makeRand(0,container.offsetHeight) + 'px'
+for(let x of Array(numRow).keys()){
+    spacesOnRow.push(x)
+}
+for(let x of Array(numCol).keys()){
+    spacesOnCol.push(x)
+}
+function shuffle(array) {
+    array.sort(function() {
+      return Math.random() - .5;
+    });
+  }
+shuffle(spacesOnRow)
+ shuffle(spacesOnCol)
+// let listWidth = []
+// let listHeight = []
+for (let i = 0; i <= 13; i++) {
 
-function makeRandNum(){
+
     const div1 = document.createElement('div')
-    
-    // div1.style.position = 'relative'
-    div1.style.position = 'absolute'
-    div1.style.border = '1px solid red'
-    div1.style.fontSize = '1.5rem'
-    div1.style.width = '35px'
-    div1.style.height = '35px'
-    div1.style.borderRadius = '50%'
-    div1.style.textAlign = 'center'
-    div1.style.padding = '0'
-    
-    div1.style.lineHeight = '1.5'
-    return div1
+    div1.classList.add('randNum');
+    div1.innerText = 1
+
+    randLeft = `${spacesOnRow[i]* tSize}px`
+    randTop = `${spacesOnCol[i]*tSize}px`
+    // randLeft = makeRand(0, container.offsetWidth) + 'px'
+    // randTop = makeRand(0, container.offsetHeight) + 'px'
+
+   
+    div1.style.left = randLeft
+    div1.style.top = randTop
+
+
+    container.append(div1)
 }
 
-    let div1 = ''
-    let listWidth = []
-    let listHeight = []
-    // let x = ''
-    for(let i = 0; i<=20;i++){
+ // listHeight.push(randTop)
+    // listWidth.push(randLeft)
 
-        div1 = makeRandNum()
-        randLeft = makeRand(0,container.offsetWidth) + 'px'
-        randTop = makeRand(0,container.offsetHeight) + 'px'
-        while(listWidth.includes(randLeft) || listHeight.includes(randTop)){
-            randLeft = makeRand(0,container.offsetWidth) + 'px'
-            randTop = makeRand(0,container.offsetHeight) + 'px'
-        }
-        listHeight.push(randTop)
-        listWidth.push(randLeft)
-        div1.style.left = randLeft
-        div1.style.top = randTop
-        // div1.innerText = 1
-        let x = document.createElement('div');
-        x.innerText = 1
-        x.style.position = 'static'
-        div1.append(x)
-        container.append(div1)
-    }
-    // div1 = makeRandNum()
-    // randLeft = makeRand(0,container.offsetWidth) + 'px'
-    // randTop = makeRand(0,container.offsetHeight) + 'px'
-    // div1.style.left = randLeft
-    // div1.style.top = randTop
-    // div1.innerText = 1
-    // container.append(div1)
+ // let x = document.createElement('div');
+    // x.innerText = 1
+    // x.style.position = 'static'
+    // div1.append(x)
 
-// const div2 = document.createElement('div')
 
-// randLeft = makeRand(0,container.offsetWidth) + 'px'
-// randTop = makeRand(0,container.offsetHeight) + 'px'
 
-// div2.innerText = 9
-// div2.style.position = 'absolute'
-// div2.style.backgroundColor = 'red'
-// div2.style.fontSize = '1.5rem'
-// div2.style.width = '35px'
-// div2.style.height = '35px'
-// div2.style.borderRadius = '50%'
-// div2.style.textAlign = 'center'
-// div2.style.lineHeight = '1.5'
-// // div2.style.padding = '0'
-// div2.style.left = randLeft
-// div2.style.top = randTop
-// container.append(div2)
 
-container.addEventListener('click', (e)=>{
+
+
+
+
+
+
+
+
+
+container.addEventListener('click', (e) => {
     console.log(e.target)
 })
