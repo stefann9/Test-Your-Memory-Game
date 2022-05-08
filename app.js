94f84ece8aa6dc9e979aa2d3e96c5d1707f384af
stef <stefan.manea198@gmail.com>
2022-05-08 15:29:51 +0300

    fix bug overlapping for more numb

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function makeRand(min, max) {
 }
 
 let tSize = 38;
-let tCount = 20;
+let tCount = 80;
 // impartim containerul in spati disponibile pe rand si coloane pentru 
 // grosimea si inaltimea numerelor
 let numRow = Math.floor(container.offsetWidth/tSize);
@@ -13,43 +13,97 @@ let numCol = Math.floor(container.offsetHeight/tSize);
 let spacesOnRow = [];
 let spacesOnCol = [];
 
+// salveaza numerele disponibile pe rand
 for(let x of Array(numRow).keys()){
     spacesOnRow.push(x)
 }
 for(let x of Array(numCol).keys()){
     spacesOnCol.push(x)
 }
+
 function shuffle(array) {
     array.sort(function() {
       return Math.random() - .5;
     });
   }
+//   amesteca spatiile disponibile
 shuffle(spacesOnRow)
- shuffle(spacesOnCol)
-// let listWidth = []
-// let listHeight = []
-for (let i = 0; i <= 13; i++) {
+shuffle(spacesOnCol)
 
+const keepWH = []
+
+// tCountloop:
+for (let i = 1; i <= tCount; i++) {
+
+    // in cazul in care marimea lui spacesOnRow si spacesOnCol este depasita adauga mai multe spatii
+    while(spacesOnCol.length < tCount || spacesOnRow.length < tCount){
+        if(spacesOnCol.length < tCount){
+            spacesOnCol.push(...spacesOnCol)
+        }else if(spacesOnRow.length < tCount){
+            spacesOnRow.push(...spacesOnRow)
+        }else{
+            break
+        }
+        
+    }
 
     const div1 = document.createElement('div')
     div1.classList.add('randNum');
-    div1.innerText = 1
+    div1.innerText = i
 
-    randLeft = `${spacesOnRow[i]* tSize}px`
+    randLeft = `${spacesOnRow[i]*tSize}px`
     randTop = `${spacesOnCol[i]*tSize}px`
-    // randLeft = makeRand(0, container.offsetWidth) + 'px'
-    // randTop = makeRand(0, container.offsetHeight) + 'px'
 
-   
+    // whileLoop:
+    keepLoop = true
+    while(keepLoop && keepWH.length>1){
+        // console.log('x')
+        for(let x of keepWH){
+             if(randLeft === x.W && randTop === x.H){
+                 console.log(randLeft === x.W && randTop === x.H)
+                 shuffle(spacesOnRow)
+                 shuffle(spacesOnCol)
+                 randLeft = `${spacesOnRow[i]*tSize}px`
+                 randTop = `${spacesOnCol[i]*tSize}px`
+                 keepLoop = true
+                 break
+             }else{
+                 keepLoop = false
+             }
+         }
+    }
+
+
+    // for(let x of keepWH){
+    //          while(randLeft === x.W && randTop === x.H){
+    //              console.log(randLeft === x.W && randTop === x.H)
+    //              shuffle(spacesOnRow)
+    //              shuffle(spacesOnCol)
+    //              randLeft = `${spacesOnRow[i]*tSize}px`
+    //              randTop = `${spacesOnCol[i]*tSize}px`
+    //          }
+    //      }
+
+    // for(let x of keepWH){
+    //     if(randLeft === x.W && randTop === x.H){
+    //         console.log(randLeft === x.W && randTop === x.H)
+    //         // shuffle(spacesOnRow)
+    //         // shuffle(spacesOnCol)
+    //         // randLeft = `${spacesOnRow[i]*tSize}px`
+    //         // randTop = `${spacesOnCol[i]*tSize}px`
+    //         continue tCountloop
+    //     }
+    // }
+
+    keepWH.push({W:randLeft,H:randTop})
+
     div1.style.left = randLeft
     div1.style.top = randTop
-
-
     container.append(div1)
 }
 
- // listHeight.push(randTop)
-    // listWidth.push(randLeft)
+ // listH.push(randTop)
+    // listW.push(randLeft)
 
  // let x = document.createElement('div');
     // x.innerText = 1
