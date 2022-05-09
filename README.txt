9b51d896ec977348e17f34c531e7f0b6897339f9
stef <stefan.manea198@gmail.com>
2022-05-09 20:38:31 +0300

    code restruct, rename var,func, add README.txt

---
+++ README.txt
@@ -0,0 +1,8 @@
+Game logic:
+
+Generates a set of numbers(as buttons) in ASC order.
+The nums are added in .container with random positions
+After 3s the nums are hidden.
+Player must choose the nums in asc order
+If succeeds, a new set +1 of nums are added.
+
--- app.js
+++ app.js
@@ -1,96 +1,200 @@
 const container = document.querySelector('.container');
+const startGameBtn = document.querySelector('button');
 
-function makeRand(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-let tSize = 38;
-let tCount = 5;
-// impartim containerul in spati disponibile pe rand si coloane pentru 
-// grosimea si inaltimea numerelor
-let numRow = Math.floor(container.offsetWidth / tSize);
-let numCol = Math.floor(container.offsetHeight / tSize);
-let spacesOnRow = [];
-let spacesOnCol = [];
-
-// salveaza numerele disponibile pe rand
-for (let x of Array(numRow).keys()) {
-    spacesOnRow.push(x)
-}
-for (let x of Array(numCol).keys()) {
-    spacesOnCol.push(x)
-}
 
 function shuffle(array) {
     array.sort(function () {
         return Math.random() - .5;
     });
 }
-//amesteca spatiile disponibile
-shuffle(spacesOnRow)
-shuffle(spacesOnCol)
-
-const keepWH = []
 
 
+function makeRand(min, max) {
+    return Math.random() * (max - min) + min;
+}
 
+function arrayInRange(num) {
+    let myArray = []
+    for (let x of Array(num).keys()) {
+        myArray.push(x)
+    }
+    return myArray
+}
 
+function ascNums(num1, num2) {
+    return num1 - 1 === num2
+}
 
+const hideNum = function (delay) {
+    return new Promise((resolve, refect) => {
+        setTimeout(() => {
+            listOfNum.forEach((x) => {
+                x.classList.add('hideNum')
+            })
+            resolve();
+        }, delay)
+    })
+}
 
+// const timeForChoosing = function(delay){
+//     return new Promise((resolve,reject)=>{
 
-
-
-
-
-
-// const listOfNum = []
-// for(let x of Array(tCount).keys()){
-//     listOfNum.push(x)
+//     })
 // }
 
+// if spacesOnCol/Row.length < numCount then add more
+function addNewSpaces(spacesOnCol,spacesOnRow,numCount){
+    while (spacesOnCol.length < numCount || spacesOnRow.length < numCount) {
+        if (spacesOnCol.length < numCount) {
+            spacesOnCol.push(...spacesOnCol)
+        } else if (spacesOnRow.length < numCount) {
+            spacesOnRow.push(...spacesOnRow)
+        } else {
+            break
+        }
+    }
+}
 
-const startGameBtn = document.querySelector('button');
+// max space occupied by num
+let numSize = 38;
+// how many num to gen
+let numCount = 2;
 
-startGameBtn.addEventListener('click', () => {
-    genRandNum(tCount)
 
-})
+// list of num in .container
+let listOfNum = []
+// list with position of nums
+let listOfPositions = []
 
-listOfDivs = []
-function genRandNum(tCount) {
-    // tCountloop:
-    for (let i = 0; i < tCount; i++) {
+let choice = NaN
+let listOfChoices = [-1]
 
-        // in cazul in care marimea lui spacesOnRow si spacesOnCol este depasita adauga mai multe spatii
-        while (spacesOnCol.length < tCount || spacesOnRow.length < tCount) {
-            if (spacesOnCol.length < tCount) {
-                spacesOnCol.push(...spacesOnCol)
-            } else if (spacesOnRow.length < tCount) {
-                spacesOnRow.push(...spacesOnRow)
-            } else {
-                break
+let score = 0;
+
+// let [spacesOnRow, spacesOnCol] = [[], []]
+// spaces available on col
+let spacesOnCol = [];
+let spacesOnRow = [];
+
+const startGame = () => {
+    resetGameVar(numCount,score);
+    
+    [spacesOnRow, spacesOnCol] = spacesOnContainer(numSize)
+
+    // avoid typig nums before hideNum
+    container.removeEventListener('mouseup', startChoosing)
+
+    genRandNum(numCount)
+
+    hideNum(3000)
+        .then(() => {
+            container.addEventListener('mouseup', startChoosing)
+        })
+}
+
+function newGame(){
+    numCount = 2
+    score = 0
+
+    startGame()
+    // remove e after newGame
+    startGameBtn.removeEventListener('click', newGame)
+}
+
+startGameBtn.addEventListener('click',newGame)
+
+
+function resetGameVar(currentCount,currentScore) {
+    container.innerText = '';
+    numCount = currentCount
+
+    listOfNum = []
+    listOfPositions = []
+
+    [spacesOnRow, spacesOnCol] = [[], []]
+
+    choice = NaN
+    listOfChoices = [-1]
+    score = currentScore
+}
+
+
+
+function spacesOnContainer(numSize) {
+    // save and return available spaces in .container
+    let numRow = Math.floor(container.offsetWidth / numSize);
+    let numCol = Math.floor(container.offsetHeight / numSize);
+
+    spacesOnRow.push(...arrayInRange(numRow))
+    spacesOnCol.push(...arrayInRange(numCol))
+
+    shuffle(spacesOnRow)
+    shuffle(spacesOnCol)
+
+    return [spacesOnRow, spacesOnCol]
+}
+
+
+
+const startChoosing = function (e) {
+
+    if (e.target.classList[0] === 'randNum' && e.target.tagName === 'BUTTON') {
+
+        choice = parseInt(e.target.innerText)
+        prevChoice = listOfChoices[listOfChoices.length - 1]
+
+        if (ascNums(choice, prevChoice)) {
+
+            listOfChoices.push(choice)
+
+            listOfNum[choice].remove()
+
+            
+            if (listOfNum.length === listOfChoices.length - 1) {
+            //win: 
+                numCount++
+                score++
+
+                startGame()
             }
-
+        } else {
+            // lose:
+            resetGameVar(numCount,score)
+            startGameBtn.addEventListener('click', newGame)
         }
 
-        const div1 = document.createElement('button')
-        div1.classList.add('randNum');
-        div1.innerText = i
+    }
 
-        randLeft = `${spacesOnRow[i] * tSize}px`
-        randTop = `${spacesOnCol[i] * tSize}px`
+}
 
-        // whileLoop:
+
+
+//gen rand num 
+function genRandNum(numCount) {
+    // tCountloop:
+    for (let i = 0; i < numCount; i++) {
+        /////////////////////////////////////////////////////////
+        // check if size of spacesOnCol/Row is exceeded
+        addNewSpaces(spacesOnCol,spacesOnRow,numCount)
+        ////////////////////////////////////////////////////////
+        const newNum = document.createElement('button')
+        newNum.classList.add('randNum');
+        newNum.innerText = i
+        /////////////////////////////////////////////////
+        //set position
+        randLeft = `${spacesOnRow[i] * numSize}px`
+        randTop = `${spacesOnCol[i] * numSize}px`
+        ////////////////////////////////////////
+        
+        // check collision
         keepLoop = true
-        while (keepLoop && keepWH.length > 1) {
-            // verifica daca noul numar se suprapune cu cele vechi
-            for (let x of keepWH) {
-                if (randLeft === x.W && randTop === x.H) {
-                    console.log(randLeft === x.W && randTop === x.H)
+        while (keepLoop && listOfPositions.length > 1) {
+            for (let x of listOfPositions) {
+                if (randLeft === x.posLeft && randTop === x.posTop) {
                     shuffle(spacesOnRow)
                     shuffle(spacesOnCol)
-                    randLeft = `${spacesOnRow[i] * tSize}px`
-                    randTop = `${spacesOnCol[i] * tSize}px`
+                    randLeft = `${spacesOnRow[i] * numSize}px`
+                    randTop = `${spacesOnCol[i] * numSize}px`
                     keepLoop = true
                     break
                 } else {
@@ -98,57 +202,63 @@ function genRandNum(tCount) {
                 }
             }
         }
-
-        keepWH.push({ W: randLeft, H: randTop })
-
-        div1.style.left = randLeft
-        div1.style.top = randTop
-        listOfDivs.push(div1)
-        container.append(div1)
+        
+        ///////////////////////////////////////////////////////////
+        listOfPositions.push({ posLeft: randLeft, posTop: randTop })
+        listOfNum.push(newNum)
+        ///////////////////////////////////////////////////////////////
+        newNum.style.left = randLeft
+        newNum.style.top = randTop
+        container.append(newNum)
+        //////////////////////////////////////////////////////////////
     }
-    setTimeout(()=>{
-        listOfDivs.forEach((x)=>{
-            x.classList.add('hideNum')
-        })
-        container.addEventListener('mouseup', startChoosing)
-    },3000)
 
+}
+
+
+
+// br
+
+
+
+
+// function spacesOnContainer(numSize,spacesColOrRow,) {
+//     // reset game var
     
-}
 
-let choice = NaN
-let listOfChoices = [-1]
-let score = 0;
+//     // impartim containerul in spati disponibile pe rand si coloane pentru 
+//     // grosimea si inaltimea numerelor
+//     let numColOrRow = Math.floor(container.offsetWidth / numSize);
+//     // let numCol = Math.floor(container.offsetHeight / numSize);
+//     // let spacesOnRow = [];
+//     // let spacesOnCol = [];
+//     // salveaza numerele disponibile pe rand/col
+//     spacesColOrRow.push(...arrayInRange(numColOrRow))
+//     // spacesOnCol.push(...arrayInRange(numCol))
+//     //amesteca spatiile disponibile
+//     shuffle(spacesColOrRow)
+//     // shuffle(spacesOnCol)
 
-const startChoosing = function(e){
-    if (e.target.classList[0] === 'randNum') {
-        choice = parseInt(e.target.innerText)
-        console.log(choice)
-
-        if (choice -1   === listOfChoices[listOfChoices.length-1]) {
-            console.log(true)
-
-            listOfChoices.push(choice)
-            console.log(listOfDivs[choice])
-            listOfDivs[choice].remove()
-            if(listOfDivs.length === listOfChoices.length - 1){
-                listOfChoices = [-1]
-                listOfDivs = []
-                tCount++
-                score++
-                genRandNum(tCount)
-            }
-            
-        } else {
-            container.innerText = '';
-            listOfChoices = [-1]
-            listOfDivs = []
-            tCount = 5
-            score = 0
-        }
-
-    }
-
-}
+//     return spacesColOrRow
+// }
 
 
+
+
+// function checkCollision(){
+//     keepLoop = true
+//     while (keepLoop && listOfPositions.length > 1) {
+//         for (let x of listOfPositions) {
+//             if (randLeft === x.posLeft && randTop === x.posTop) {
+//                 shuffle(spacesOnRow)
+//                 shuffle(spacesOnCol)
+//                 randLeft = `${spacesOnRow[i] * numSize}px`
+//                 randTop = `${spacesOnCol[i] * numSize}px`
+//                 keepLoop = true
+//                 break
+//             } else {
+//                 keepLoop = false
+//             }
+//         }
+//     }
+// }
